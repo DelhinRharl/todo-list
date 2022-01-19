1c84434e3546a51d8cc24bae9ea971a0e337ad53
Affaxed Kiprotich <delhinrharl@gmail.com>
2022-01-19 09:31:42 +0100

    Add files to the repo

---
+++ src/functionBased/pages/About.js
@@ -0,0 +1,12 @@
+// import React from "react"
+// import { Link, useRouteMatch, Route } from "react-router-dom"
+// import SinglePage from "./SinglePage"
+// const About = () => {
+//   console.log(useRouteMatch())
+//   return (
+//     <div>
+//         <p>hello</p>
+//     </div>
+//   )
+// }
+// export default About
